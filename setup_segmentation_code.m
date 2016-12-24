16eead15f556e2c8661561a194428882578b5d39
José Ignacio Orlando <ignaciorlando@gmail.com>
2016-12-24 10:41:47 -0300

    setup updated

--- setup_segmentation_code.m
+++ setup_segmentation_code.m
@@ -1,6 +1,7 @@
 
 root = pwd;
 
+% Add folders
 addpath(genpath(fullfile(root, 'Features')));
 addpath(genpath(fullfile(root, 'Preprocessing')));
 addpath(genpath(fullfile(root, 'Segmentations')));
@@ -8,4 +9,9 @@ addpath(genpath(fullfile(root, 'SOSVM')));
 addpath(genpath(fullfile(root, 'Util', 'Evaluation')));
 addpath(genpath(fullfile(root, 'Util', 'vlfeat', 'toolbox')));
 
+% Setup VLFeat
 vl_setup;
+
+% Compile relevant files
+mex ./CRF/CRF_1.0/fullyCRFwithGivenPairwises.cpp ./CRF/CRF_1.0/densecrf.cpp ./CRF/CRF_1.0/util.cpp 
+mex ./CRF/CRF_1.0/pairwisePart.cpp ./CRF/CRF_1.0/densecrf.cpp ./CRF/CRF_1.0/util.cpp
